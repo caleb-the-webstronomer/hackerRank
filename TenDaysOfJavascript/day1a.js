68af2e3cacd29ae7dfe5d0fca8b6f3236dab950c
Caleb Thompson <caleb@webstronomers.com>
2020-02-27 11:40:34 -0800

    day 1 days of js

---
+++ TenDaysOfJavascript/day1a.js
@@ -0,0 +1,15 @@
+//  :::::::: A R I T H M E T I C  :::  O P E R A T O R S
+
+// https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem
+
+function getArea(length, width) {
+  let area;
+  area = length * width;
+  return area;
+}
+
+function getPerimeter(length, width) {
+  let perimeter;
+  perimeter = 2 * (length + width);
+  return perimeter;
+}
